320ea508a972c41c03e5fbfe598a25a95bf0bb15
Omar Badr <69204285+Grintaking19@users.noreply.github.com>
2026-06-08 12:39:40 +0300

    Add: two-integer-sum - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum/submission-11.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_index_map = {}
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in num_index_map and i != num_index_map[diff] :
+                return [min(i, num_index_map[diff]), max(i, num_index_map[diff])]
+            num_index_map[n] = i
+        return []
